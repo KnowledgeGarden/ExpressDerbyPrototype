4bfb0d38974faf05d035e5de12c59db77e326342
Jack Park <jackpark@gmail.com>
2014-06-01 17:41:06 -0700

    minor code cleanup

--- apps/models/subjectproxy.js
+++ apps/models/subjectproxy.js
@@ -14,10 +14,10 @@ SubjectProxy.prototype.getLocator = function() {
 };
 SubjectProxy.prototype.setResourceUrl = function(url) {
 	this.properties['url'] = url;
-}
+};
 SubjectProxy.prototype.getResourceUrl = function() {
 	return this.properties['url'];
-}
+};
 SubjectProxy.prototype.addLabel = function(label, language) {
 	var lan = language;
 	if (!lan)
@@ -27,7 +27,7 @@ SubjectProxy.prototype.addLabel = function(label, language) {
 		lx = new [];
 	lx.push(label);
 	this.properties['label',lan] = lx;
-}
+};
 SubjectProxy.prototype.addDetails = function(details,language) {
 	var lan = language;
 	if (!lan)
@@ -38,19 +38,19 @@ SubjectProxy.prototype.addDetails = function(details,language) {
 	lx.push(details);
 	this.properties['details',lan] = lx;
 	
-}
+};
 SubjectProxy.prototype.listLabels = function(language) {
 	var lan = language;
 	if (!lan)
 		lan = 'en'; // default
 	return this.properties['label',lan];
-}
+};
 SubjectProxy.prototype.listDetails = function(language) {
 	var lan = language;
 	if (!lan)
 		lan = 'en'; // default
 	return this.properties['details',lan];
-}
+};
 SubjectProxy.prototype.toJSON = function() {
 	//TODO
 	return 'test:foo';
